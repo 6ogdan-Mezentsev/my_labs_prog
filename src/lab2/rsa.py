7031d13cee11e8c06d754998c98422971f7d2694
Bogdan Mezentsev <mezentsev.6ogdan@yandex.ru>
2024-10-05 21:19:01 +0300

    add RSA cipher

--- src/lab2/rsa.py
+++ src/lab2/rsa.py
@@ -12,8 +12,14 @@ def is_prime(n: int) -> bool:
     >>> is_prime(8)
     False
     """
-    # PUT YOUR CODE HERE
-    pass
+    k = 0
+    for i in range(1, n+1):
+        if n % i == 0:
+            k += 1
+    if k == 2:
+        return True
+    else:
+        return False
 
 
 def gcd(a: int, b: int) -> int:
@@ -24,8 +30,11 @@ def gcd(a: int, b: int) -> int:
     >>> gcd(3, 7)
     1
     """
-    # PUT YOUR CODE HERE
-    pass
+    remainder = 0
+    while max(a, b) % min(a, b) != 0:
+        remainder = max(a, b) % min(a, b)
+        a, b = min(a, b), remainder
+    return remainder
 
 
 def multiplicative_inverse(e: int, phi: int) -> int:
@@ -35,8 +44,9 @@ def multiplicative_inverse(e: int, phi: int) -> int:
     >>> multiplicative_inverse(7, 40)
     23
     """
-    # PUT YOUR CODE HERE
-    pass
+    for b in range(0, phi):
+        if e * b % phi == 1:
+            return b
 
 
 def generate_keypair(p: int, q: int) -> tp.Tuple[tp.Tuple[int, int], tp.Tuple[int, int]]:
@@ -45,11 +55,9 @@ def generate_keypair(p: int, q: int) -> tp.Tuple[tp.Tuple[int, int], tp.Tuple[in
     elif p == q:
         raise ValueError("p and q cannot be equal")
 
-    # n = pq
-    # PUT YOUR CODE HERE
+    n = p*q
 
-    # phi = (p-1)(q-1)
-    # PUT YOUR CODE HERE
+    phi = (p-1)*(q-1)
 
     # Choose an integer e such that e and phi(n) are coprime
     e = random.randrange(1, phi)
