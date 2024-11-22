7da6522df7af696aebb80e7e6e393b3183067a54
6ogdan-Mezentsev <mezentsev.6ogdan@yandex.ru>
2024-11-22 22:05:58 +0300

    Revert "Lab 1.2"

--- src/lab2/caesar.py
+++ src/lab2/caesar.py
@@ -10,24 +10,8 @@ def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     >>> encrypt_caesar("")
     ''
     """
-
     ciphertext = ""
-
-    for i in range(len(plaintext)):
-        if plaintext[i].isupper():
-            cipherchar = ord(plaintext[i]) + shift
-            if cipherchar > 90:
-                while cipherchar > 90:
-                    cipherchar -= 26
-            ciphertext += chr(cipherchar)
-        elif plaintext[i].islower():
-            cipherchar = ord(plaintext[i]) + shift
-            if cipherchar > 122:
-                while cipherchar > 122:
-                    cipherchar -= 26
-            ciphertext += chr(cipherchar)
-        elif (plaintext[i].isdigit()) or (not plaintext[i].isalpha() and not plaintext[i].isdigit()):
-            ciphertext += plaintext[i]
+    # PUT YOUR CODE HERE
     return ciphertext
 
 
@@ -44,20 +28,5 @@ def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     ''
     """
     plaintext = ""
-
-    for i in range(len(ciphertext)):
-        if ciphertext[i].isupper():
-            plainchar = ord(ciphertext[i]) - shift
-            if plainchar < 65:
-                while plainchar < 65:
-                    plainchar += 26
-            plaintext += chr(plainchar)
-        elif ciphertext[i].islower():
-            plainchar = ord(ciphertext[i]) - shift
-            if plainchar < 97:
-                while plainchar < 97:
-                    plainchar += 26
-            plaintext += chr(plainchar)
-        elif (ciphertext[i].isdigit()) or (not ciphertext[i].isalpha() and not ciphertext[i].isdigit()):
-            plaintext += ciphertext[i]
+    # PUT YOUR CODE HERE
     return plaintext
--- src/lab2/vigenre.py
+++ src/lab2/vigenre.py
@@ -9,27 +9,7 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     'LXFOPVEFRNHR'
     """
     ciphertext = ""
-    i = 0
-    j = 0
-    while i != len(plaintext):
-        if plaintext[i].isupper():
-            cipherchar = ord(plaintext[i]) + ord(keyword[j]) - 65
-            if cipherchar > 90:
-                while cipherchar > 90:
-                    cipherchar -= 26
-            ciphertext += chr(cipherchar)
-        elif plaintext[i].islower():
-            cipherchar = ord(plaintext[i]) + ord(keyword[j]) - 97
-            if cipherchar > 122:
-                while cipherchar > 122:
-                    cipherchar -= 26
-            ciphertext += chr(cipherchar)
-        elif (plaintext[i].isdigit()) or (not plaintext[i].isalpha() and not plaintext[i].isdigit()):
-            ciphertext += plaintext[i]
-        j += 1
-        if j == len(keyword):
-            j = 0
-        i += 1
+    # PUT YOUR CODE HERE
     return ciphertext
 
 
@@ -44,25 +24,5 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     'ATTACKATDAWN'
     """
     plaintext = ""
-    i = 0
-    j = 0
-    while i != len(ciphertext):
-        if ciphertext[i].isupper():
-            plainchar = ord(ciphertext[i]) - ord(keyword[j]) + 65
-            if plainchar < 65:
-                while plainchar < 65:
-                    plainchar += 26
-            plaintext += chr(plainchar)
-        elif ciphertext[i].islower():
-            plainchar = ord(ciphertext[i]) - ord(keyword[j]) + 97
-            if plainchar < 97:
-                while plainchar < 97:
-                    plainchar += 26
-            plaintext += chr(plainchar)
-        elif (ciphertext[i].isdigit()) or (not ciphertext[i].isalpha() and not ciphertext[i].isdigit()):
-            plaintext += ciphertext[i]
-        j += 1
-        if j == len(keyword):
-            j = 0
-        i += 1
+    # PUT YOUR CODE HERE
     return plaintext
